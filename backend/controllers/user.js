bb67ff60bc1f22c44df5a432c54a683fcd217656
Loic2dot0 <moreaux.loic@gmail.com>
2021-11-03 12:11:06 +0100

    controllers/user.js : création du middleware login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 const User = require('../models/User');
+const UserLog = require('../models/UserLog');
 
 
 exports.signup = (req, res, next) => {
@@ -20,7 +21,32 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-
+    User.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        .then(user => {
+            if(user === null){
+                return res.status(401).json({error: 'Utilisateur non trouvé !'});
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if(!valid){ 
+                        return res.status(401).json({error: 'Mot de passe incorrect !'});
+                    }
+                    res.status(200).json({
+                        userId: user.id_user,
+                        token: jwt.sign( //fonction de Jsontoken
+                            {userId: user.id_user}, // données a encoder
+                            config.tokenKey , //clé d'encodage
+                            {expiresIn: '24h'} // expiration du token
+                        )
+                    }); 
+                })
+                .catch(error => res.status(500).json({error}));
+        })
+        .catch(error => res.status(500).json({error}));
 };
 
 exports.getUser = (req, res, next) => {
