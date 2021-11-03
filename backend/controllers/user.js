95c68f2658dbb5d167d28272d42d9a6cbaba5393
Loic2dot0 <moreaux.loic@gmail.com>
2021-11-03 10:46:58 +0100

    controllers/user.js : création du middleware signup

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,22 @@
-exports.signup = (req, res, next) => {
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+const User = require('../models/User');
 
+
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+        .then(hash =>{
+            User.create({
+                email: req.body.email,
+                password: hash,
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                service: req.body.service })
+                .then(()=> res.status(201).json({message: 'Utilisateur créé !'}))
+                .catch((error)=> res.status(500).json({error}));
+        })
+        .catch(error => res.status(500).json({error}));
 };
 
 exports.login = (req, res, next) => {
