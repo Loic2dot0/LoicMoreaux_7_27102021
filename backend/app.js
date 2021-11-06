337f643f135bc972ef74518e8f46f1e4b21eea12
Loic2dot0 <moreaux.loic@gmail.com>
2021-11-06 15:41:27 +0100

    app.js : modification de la synchronisation des models de sequelize

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sequelize = require('./config/sequelize');
 const Models = require('./models/ExportModels');
+const path = require('path');
 
 const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/post');
@@ -9,34 +10,9 @@ const commentRoutes = require('./routes/comment');
 
 sequelize.authenticate()
     .then(() => {
-        Models.User.sync()
-            .then(()=>console.log('Users table synchronized!'))
-            .catch((error)=> console.log(error));
-        Models.UserLog.sync()
-            .then(()=>console.log('Users_Log table synchronized!'))
-            .catch((error)=> console.log(error));
-        Models.Moderator.sync()
-            .then(()=>console.log('Moderators table synchronized!'))
-            .catch((error)=> console.log(error));
-        Models.Post.sync()
-            .then(()=>console.log('Posts table synchronized!'))
-            .catch((error)=> console.log(error));
-        Models.PostModerate.sync()
-            .then(()=>console.log('Posts_moderate table synchronized!'))
-            .catch((error)=> console.log(error));
-        Models.PostLike.sync()
-            .then(()=>console.log('Posts_likes table synchronized!'))
-            .catch((error)=> console.log(error));
-        Models.PostDislike.sync()
-            .then(()=>console.log('Posts_dislikes table synchronized!'))
-            .catch((error)=> console.log(error));
-        Models.Comment.sync()
-            .then(()=>console.log('Comments table synchronized!'))
-            .catch((error)=> console.log(error));
-        Models.CommentModerate.sync()
-            .then(()=>console.log('Comments_moderate table synchronized!'))
-            .catch((error)=> console.log(error));        
-        console.log('Successfull to access database');
+        sequelize.sync()
+            .then(()=>console.log('All models were synchronized successfully !'))
+            .catch((error)=> console.log(error)); 
     })
     .catch((error)=> console.log(`Failed to access database : ${error}`));
 
