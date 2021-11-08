b6f83b76a4041c25bb1e288326c72f4ede9e3626
Loic2dot0 <moreaux.loic@gmail.com>
2021-11-08 11:47:43 +0100

    controllers/comment.js : création du middleware createComment

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -1,6 +1,8 @@
 const config = require('../config/config');
 const sequelize = require('../config/sequelize');
 const { QueryTypes } = require('../config/sequelize');
+const Comment = require('../models/Comment')
+
 
 exports.getAllComment = (req, res, next) => {
     sequelize.query('SELECT * FROM comments WHERE id_post = :id_post AND  NOT id_comment = (SELECT id_comment FROM comments_moderate) ORDER BY createdAt ASC',
@@ -12,7 +14,13 @@ exports.getAllComment = (req, res, next) => {
 };
 
 exports.createComment = (req, res, next) => {
-
+    Comment.create({
+        id_post : req.params.id_post,
+        id_user: req.body.id_user,
+        comment: req.body.comment 
+    })
+    .then(()=> res.status(201).json({message: 'Commentaire créé !'}))
+    .catch((error)=> res.status(500).json({error}));
 };
 
 exports.modifyComment = (req, res, next) => {
