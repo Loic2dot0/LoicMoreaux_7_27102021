f191622a3ec9b0d188e6e500ccd5246b6945c22a
Loic2dot0 <moreaux.loic@gmail.com>
2021-11-08 11:25:45 +0100

    controllers/comments.js : création du middleware getAllComment

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -1,5 +1,14 @@
-exports.getAllComment = (req, res, next) => {
+const config = require('../config/config');
+const sequelize = require('../config/sequelize');
+const { QueryTypes } = require('../config/sequelize');
 
+exports.getAllComment = (req, res, next) => {
+    sequelize.query('SELECT * FROM comments WHERE id_post = :id_post AND  NOT id_comment = (SELECT id_comment FROM comments_moderate) ORDER BY createdAt ASC',
+        { 
+            replacements: { id_post : req.params.id_post },
+            type: QueryTypes.SELECT })
+        .then(comments => res.status(200).json(comments))
+        .catch(error => res.status(400).json({error}));
 };
 
 exports.createComment = (req, res, next) => {
