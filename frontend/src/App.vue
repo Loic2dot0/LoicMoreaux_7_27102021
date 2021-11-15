dc0a6a374a43b7c0b3fb5b40612e7bd904b6f59a
Loic2dot0 <moreaux.loic@gmail.com>
2021-11-15 16:08:23 +0100

    Frontend - App.vue: ajout du style général. Header.vue: création du fichier et mise en forme du header. Ajout du fichier image pour le header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,38 @@
 <template>
   <div id="app">
+    <header-main></header-main>
     
   </div>
 </template>
 
 <script>
-
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
+    'header-main': Header
     
   }
 }
 </script>
 
 <style>
+  html{
+    font-size: 100%;
+  }
 
+  body{
+    background-color: #000;
+    margin: 0;
+    color: #fff;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  body *{
+    box-sizing: border-box;
+  }
 </style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <header>
+        <h1>Groupomania</h1>
+        
+        <a href="index.html" class="header_a">
+            <img src="../assets/images/groupomamia_logo.png">
+        </a>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+    
+}
+</script>
+
+<style scoped>
+    header{
+        background-color: #333;
+        margin-bottom: 30px;
+    }
+
+    h1{
+        display:none;
+    }
+
+    a{
+        display: block;
+        width: 80%;
+        max-width: 1200px;
+        margin: auto;
+        text-align:  center;
+        border-bottom: solid 3px #ffd7d7;
+    }
+
+    img{
+        width:70%;
+        max-width: 800px;
+        padding: 10px;
+    }
+</style>
