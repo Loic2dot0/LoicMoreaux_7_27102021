13bc5b6ee5a0aea7c5d0e28e5d32cd352fe44907
Loic2dot0 <moreaux.loic@gmail.com>
2021-11-17 19:54:41 +0100

    Frontend - Profile.vue: modification du template, création des scripts

--- frontend/src/components/layouts/Profile.vue
+++ frontend/src/components/layouts/Profile.vue
@@ -1,11 +1,12 @@
 <template>
     <aside>
         <button>Publier</button>
-        <img src="../../assets/images/avatar.jpg">
+        <img v-if="user.avatar" v-bind:src="user.avatar">
+        <img v-else src="../../assets/images/avatar.jpg">
         <p>
-            {{ Prénom }}<br>
-            {{ Nom }}<br>
-            {{ Service }}
+            {{ user.firstname }}<br>
+            {{ user.lastname }}<br>
+            {{ user.service }}
         </p>
         <button>Mon profil</button>
         <button>Se déconnecter</button>
@@ -13,8 +14,31 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'Profile'    
+    name: 'Profile',
+    data(){
+        return{
+            user: {
+                firstname: null,
+                lastname: null,
+                service: null,
+                avatar: null
+            }
+        }
+    },
+    created(){
+        const userId = JSON.parse(sessionStorage.userAuth).userId;
+        //const token = JSON.parse(sessionStorage.token);
+
+        axios.get(`http://localhost:3000/api/auth/profile/${userId}`)
+            .then(res =>{
+               console.log(res.data);
+               this.user = {...res.data};
+            })
+            .catch(error=> console.log(error));
+    }    
 }
 </script>
 
