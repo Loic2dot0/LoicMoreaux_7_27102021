8599d16f288b92247093c866b3dcc6e88d86860e
Loic2dot0 <moreaux.loic@gmail.com>
2021-11-17 17:49:33 +0100

    Frontend - Login.vue:  modification du template et du style, création des methods

--- frontend/src/components/layouts/Login.vue
+++ frontend/src/components/layouts/Login.vue
@@ -1,20 +1,80 @@
 <template>
-    <form>
-        <label for="mail">E-mail</label>
-        <input type="text" name="mail">
-        <label for="password">Mot de passe</label>
-        <input type="text" name="password">
+    <form v-on:submit="login">
+        <span v-if="error.global">{{ error.global }}</span>
+        <label for="email">E-mail : <span v-if="error.email">{{ error.email }}</span></label>
+        <input type="text" name="email" id="email" v-model="formData.email">
+        <label for="password">Mot de passe : <span v-if="error.password">{{ error.password }}</span></label>
+        <input type="text" name="password" id="password" v-model="formData.password"> 
         <input type="submit" value="Se connecter" class="btnSubmit">
     </form>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'Login'
-    
+    name: 'Login',
+    data(){
+        return{
+            error: {
+                email: null,
+                password: null,
+                global: null
+            },
+            formData:{
+                email: null,
+                password: null
+            }
+        }
+    },
+    methods:{
+        login: function(e){
+            e.preventDefault();
+            this.error.email = null;
+            this.error.password = null;
+            this.error.global = null;
+            const vm = this;
+            
+            if(!this.formData.email){
+                this.error.email = 'Vous devez entrer votre adresse mail !';
+            }
+
+            if(!this.formData.password){
+                this.error.password = 'Vous devez entrer votre mot de passe !';
+            }           
+
+            if(!this.error.email && !this.error.password){
+                axios({
+                        url: 'http://localhost:3000/api/auth/login',
+                        method: 'POST',
+                        headers: { 
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        data: JSON.stringify({
+                            email: this.formData.email,
+                            password: this.formData.password
+                        })                    
+                    })
+                    .then(function(res){
+                        console.log(res.data);
+                        sessionStorage.setItem('userAuth', JSON.stringify(res.data));
+                        document.location.href = './';
+                    })
+                    .catch(function(error){
+                        let errormessage = error.response.data.error;
+                        console.log(errormessage);
+                        vm.error.global = errormessage;
+                    });
+            }
+        }
+    }
 }
 </script>
 
 <style scoped>
-  
+  span{
+      color: #fd2d01;
+      text-align:center;   
+  }
 </style>
