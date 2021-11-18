de4bd1c137b8370aa6470b42f304cad6f96e6e57
Loic2dot0 <moreaux.loic@gmail.com>
2021-11-18 12:09:58 +0100

    Frontend - Error.vue : création du component et de son contenu

---
+++ frontend/src/components/Error.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <h2>{{ errorStatus }}</h2>
+        <p>Oups... Une errreur est survenue</p>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'ErrorPage',
+    data(){
+        return{
+            errorStatus: this.$route.params.status
+        }
+    },
+    created(){
+        console.log(this.$route.params.status);
+    }
+}
+</script>
+
+<style scoped>
+    section{
+        background-color: #333;
+        padding: 50px;
+        font-size: 1.2rem;
+    }
+    h2{
+        color: #fd2d01;
+        font-size: 5rem;
+        margin-top: 20px;
+    }
+</style>
