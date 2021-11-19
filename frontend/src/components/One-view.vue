3722221e190e33f87d47478d5032487af2b7dcc6
Loic2dot0 <moreaux.loic@gmail.com>
2021-11-19 10:42:19 +0100

    Frontend - One-view.vue: création du fichier et de son contenu. routes.js: modification des routes pour le nouveau fichier

---
+++ frontend/src/components/One-view.vue
@@ -0,0 +1,50 @@
+<template>
+    <div v-if="session">
+        <profile></profile> 
+        <onepost></onepost>
+    </div>
+</template>
+
+<script>
+import Profile from './layouts/Profile.vue';
+import OnePost from './layouts/Onepost.vue';
+
+export default {
+    name: 'One-view',
+    data(){
+        return{
+            session: false
+        }
+    },
+    methods:{
+        
+    },
+    components: {
+        'profile' : Profile,
+        'onepost' : OnePost      
+    },
+    created(){
+        if(!sessionStorage.userAuth){
+            document.location.href = '/login';
+        }else this.session = true;
+    }
+}
+
+</script>
+
+<style scoped>
+    div{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    
+    @media screen and (min-width: 1024px) {
+        div{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+            align-items: start;
+        }
+    }
+</style>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,11 +1,11 @@
 import MainView from './components/Main-view';
 import Account from './components/Account';
 import ErrorPage from './components/Error';
-import OnePost from './components/OnePost'; 
+import OneView from './components/One-view'; 
 
 export default [
     {path: '/', component: MainView},
     {path: '/login', component: Account},
     {path: '/error/:status', component: ErrorPage},
-    {path: '/post/:id_post', component: OnePost},
+    {path: '/post/:id_post', component: OneView},
 ];
