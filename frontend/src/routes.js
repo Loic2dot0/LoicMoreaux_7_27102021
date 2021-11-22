024d1543b74ba6417856d3567a6bd194f5546e7e
Loic2dot0 <moreaux.loic@gmail.com>
2021-11-22 17:08:44 +0100

    Frontend - routes.js: définitioon de la route pour la modification du profil de l'utlisateur

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,11 +1,13 @@
 import MainView from './components/Main-view';
 import Account from './components/Account';
+import ProfileModify from './components/Profilemodify';
 import ErrorPage from './components/Error';
 import OneView from './components/One-view'; 
 
 export default [
     {path: '/', component: MainView},
     {path: '/login', component: Account},
+    {path: '/profile',  component: ProfileModify},
     {path: '/error/:status', component: ErrorPage},
     {path: '/post/:id_post', component: OneView},
 ];
