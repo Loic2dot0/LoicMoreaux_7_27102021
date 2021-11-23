6365c4650d184228fb402392d1ebd7ffeb8e50a3
Loic2dot0 <moreaux.loic@gmail.com>
2021-11-23 16:38:15 +0100

    Frontend - routes.js: création de la route pour la création de publication, modification de la route pour l'affichage d'une publication

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,12 +2,14 @@ import MainView from './components/Main-view';
 import Account from './components/Account';
 import ProfileModify from './components/Profilemodify';
 import ErrorPage from './components/Error';
-import OneView from './components/One-view'; 
+import OneView from './components/One-view';
+import PostCreate from './components/Post-create';  
 
 export default [
     {path: '/', component: MainView},
     {path: '/login', component: Account},
     {path: '/profile',  component: ProfileModify},
     {path: '/error/:status', component: ErrorPage},
-    {path: '/post/:id_post', component: OneView},
+    {path: '/post/view/:id_post', component: OneView},
+    {path: '/post/create', component: PostCreate}
 ];
