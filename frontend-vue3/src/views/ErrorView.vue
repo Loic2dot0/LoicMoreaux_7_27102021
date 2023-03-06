6a803cf4ec1400e0620e204a79f6c74a8d091f26
Loic2dot0 <moreaux.loic@gmail.com>
2023-03-06 16:50:14 +0100

    Ajout de ErrorView

---
+++ frontend-vue3/src/views/ErrorView.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <h2>{{ errorStatus }}</h2>
+        <p>Oups... Une errreur est survenue</p>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "ErrorView",
+        data(){
+            return{
+                errorStatus: this.$route.params.status
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    section{
+        background-color: #333;
+        padding: 50px;
+        font-size: 1.2rem;
+        margin:auto;
+    }
+    
+    h2{
+        color: #fd2d01;
+        font-size: 5rem;
+        margin-top: 20px;
+    }
+</style>
