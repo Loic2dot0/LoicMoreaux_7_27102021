6c563ddddc27f044adf66359865472a6265da7a0
Loic2dot0 <moreaux.loic@gmail.com>
2023-03-06 16:43:43 +0100

    Ajout de Profile et Feed

---
+++ frontend-vue3/src/components/TheFeed.vue
@@ -0,0 +1,233 @@
+<template :key="reload">
+    <section>
+        <article v-if="posts.length == 0">
+            <p>Désolé il n'y a aucune publication pour le moment...</p>
+        </article>
+        <article v-else :key="index" v-for="(post, index) in posts">
+            <div class="option" v-if="post.User.id_user == userId || moderator">
+                <router-link v-if="post.User.id_user == userId" :to="`/post/modify/${post.id_post}`" class="btn-modify">Éditer</router-link>
+                <button v-if="post.User.id_user == userId" class="btn-delpost" @click="modalDelete(post.id_post)">Supprimer</button>
+                <button v-if="moderator" class="btn-delpost" @click="moderatePost(post.id_post)">Modérer</button>
+            </div>
+            <div class="author">
+                <img v-if="post.User.avatar" :src="post.User.avatar" class="author__avatar" alt="avatar de l'utilisateur">
+                <img v-else src="../assets/images/avatar.jpg" class="author__avatar" alt="avatar de l'utilisateur">
+                <div class="author__text">
+                    <p>{{ post.User.firstname }} {{ post.User.lastname }} <i>{{ post.User.service }}</i></p>
+                    <p class="author__date">{{ formatDate(post.createdAt, post.updatedAt) }}</p>
+                </div>
+            </div>
+            <h2 class="article__title">{{ post.title }}</h2>
+            
+            <router-link :to="`/post/view/${post.id_post}`">
+                <div class="article__img">
+                    <img :src="post.image_url" :alt="post.title"> 
+                </div>
+            </router-link>
+            <Reaction :id_post="post.id_post" :moderator="moderator"></Reaction>           
+        </article>
+        <ModalDelPost v-if="modal" @delete="deletePost(deleteidpost)" @close="modal=false"></ModalDelPost>   
+    </section>
+</template>
+
+<script>
+    import axios from 'axios';
+    import config from '../config.js';
+    import Reaction from './Reaction.vue';
+    import ModalDelPost from './ModalDelPost.vue';
+
+    export default {
+        name: 'Feed',
+        props: {
+            moderator: {
+                type: Boolean,
+                required: true
+            }
+        },
+        components: {
+            Reaction,
+            ModalDelPost      
+        },
+        data() {
+            return {
+                posts: [],
+                userId: null,
+                modal:false,
+                deleteidpost: null
+            }
+        },
+        methods: {
+            formatDate(createdDate, updatedate){
+                const dateISO = new Date(updatedate)
+                const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute:'numeric', second:'numeric' };
+                let date = dateISO.toLocaleDateString('fr-FR', options);
+                
+                if(createdDate == updatedate){
+                    return `Publié le ${date}`;
+                }else return `Modifié le ${date}`;
+            },
+            modalDelete(id_post){
+                this.deleteidpost = id_post;
+                this.modal = true;
+            },
+            deletePost(id_post){
+                const token = JSON.parse(sessionStorage.userAuth).token;
+                const vm = this;
+
+                axios.delete(`${config.urlApi}/api/posts/${id_post}`,{
+                    headers:{'authorization' : `Bearer ${token}`}
+                })
+                .then(()=> vm.$emit('reload'))
+                .catch(error=> {
+                    if(error.response.status > 400){
+                        document.location.href = `/error/${error.response.status}`;
+                    }
+                });
+            },
+            moderatePost(id_post){
+                const token = JSON.parse(sessionStorage.userAuth).token;
+                const vm = this;
+
+                axios.post(`${config.urlApi}/api/posts/${id_post}/moderate`,
+                    JSON.stringify({ moderate: true }),
+                    {
+                        headers: { 
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                            'authorization' : `Bearer ${token}`
+                        }                     
+                    })
+                    .then(()=> vm.$emit('reload'))
+                    .catch(error=> {
+                        if(error.response.status > 400){
+                            document.location.href = `/error/${error.response.status}`;
+                        }
+                    });
+            }
+        },
+        created(){
+            const token = JSON.parse(sessionStorage.userAuth).token;
+            this.userId = JSON.parse(sessionStorage.userAuth).userId;
+
+            axios.get(`${config.urlApi}/api/posts`,{
+                    headers:{'authorization' : `Bearer ${token}`}
+                })
+                .then(res =>{
+                this.posts = res.data;
+                })
+                .catch(error=> {
+                if(error.response.status > 400){
+                        document.location.href = `/error/${error.response.status}`;
+                    }
+                });    
+        }
+    }
+</script>
+
+<style>
+    section{
+        width: 95%;
+        margin-top: 20px;
+    }
+
+    article{
+        width: 100%;
+        background-color: #333;
+        margin-bottom: 15px;
+        padding: 5px;
+    }
+
+    .option{
+        display: flex;
+        justify-content: flex-end;
+    }
+
+    .author{
+        display: flex;
+    }
+
+    .author__avatar{
+        width: 40px;
+        height: 40px;
+        object-fit: cover;
+        object-position: 50% 50%;
+        overflow: hidden;
+        border-radius: 5px;
+    }
+
+    .author__text{
+        margin-left: 5px;
+        flex: 1;
+        font-size: 1.2rem;
+        overflow-wrap: break-word;
+    }
+
+    .author__text p{
+        margin: 0;
+    }
+    
+    .author__date{
+        font-size: 1rem
+    }
+
+    .article__title{
+        margin: 0;
+        font-weight: normal;
+        overflow-wrap: break-word; 
+    }
+
+    .article__img{
+        text-align: center;
+        margin-top: 5px;
+    }
+
+    .article__img img{
+        max-width: 100%;
+        max-height: 350px;
+        margin: auto;
+    }
+    
+    .btn-modify, .btn-delpost{
+        padding: 5px;
+        border-radius: 5px;
+        background-color: #fff;
+        color: #000;
+        font-style: italic;   
+    }
+
+    .btn-modify:hover{
+        background-color: #000;
+        color: #fff;
+        transition: 400ms;
+        text-decoration: none;
+    }
+
+    .btn-delpost{
+        border: none;
+        margin-left: 5px;
+        font-size: 1rem;
+        background-color: #f1aeb5;
+    }
+    
+    .btn-delpost:hover{
+        color: #fff;
+        background-color: #842029;
+        transition: 400ms;
+    }
+
+    @media screen and (min-width: 1024px) {
+        section{
+            width: 67%;
+            margin-top: 0px
+        }
+        
+        .author__text{
+            font-size: 1.4rem;
+        }
+
+        .btn-modify, .btn-delpost{
+            width: 15%;
+            text-align: center;
+        }
+    }
+</style>
---
+++ frontend-vue3/src/views/ProfileView.vue
@@ -0,0 +1,268 @@
+<template>
+    <div v-if="session" class="div-global">
+        <Profile :key="reload"></Profile>
+        <section>
+            <div>
+                <h2>Modifier mon profil</h2>
+
+                <form @submit="modifyProfile">
+                    <span class="success" v-if="message">{{ message }}</span>
+                    <input type="file" accept="image/png, image/jpeg, image/jpg, image/webp" ref="file" @change="upload()">
+                    <span class="error" v-if="error.global">{{ error.global }}</span>
+                    <label for="firstname">Prénom : <span class="error" v-if="error.firstname">{{ error.firstname }}</span></label>
+                    <input type="text" name="firstname" maxlength="25"
+                        v-model="formData.firstname"
+                        @keyup="validText(formData.firstname, 'firstname')"
+                        :class="{valid: valid.firstname}"
+                    >
+                    <label for="lastname">Nom : <span class="error" v-if="error.lastname">{{ error.lastname }}</span></label>
+                    <input type="text" name="lastname" maxlength="25"
+                        v-model="formData.lastname"
+                        @keyup="validText(formData.lastname, 'lastname')"
+                        :class="{valid: valid.lastname}"
+                    >
+                    <label for="service">Service : </label>
+                    <select name="service" v-model="formData.service">
+                        <option :key="index" v-for="(service, index) in serviceList" >{{ service }}</option>
+                    </select>
+                    <input type="submit" value="Modifier mon compte" class="btnSubmit" :disabled=btnDisabled>
+                </form>
+            </div>
+
+            <div class="div-delete">
+                <button @click="modal=true" class="btn-delete">Supprimer mon compte</button>
+            </div>
+        </section>
+        <ModalDelete v-if="modal" @close="modal=false"></ModalDelete>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    import config from '../config';
+    import Profile from '../components/TheProfile.vue';
+    import ModalDelete from '../components/ModalDelete.vue';
+
+    export default {
+        name: 'ProfileView',
+        components: {
+            Profile,
+            ModalDelete
+        },
+        data(){
+            return{
+                session: false,
+                message: null,
+                reload: 0,
+                modal: false,
+                formData: {
+                    firstname: null,
+                    lastname: null,
+                    service: null,
+                    file: null
+                },
+                serviceList: ['Commercial', 'Marketing', 'Logistique', 'Ressource Humaine', 'Relation Client', ' Financier'],
+                error: {
+                    global: null,
+                    firstname: null,
+                    lastname: null
+                },
+                valid:{
+                    firstname: true,
+                    lastname: true
+                }
+            }
+        },
+        computed: {
+            btnDisabled(){
+                if(this.valid.firstname && this.valid.lastname && this.formData.service != ''){
+                    return false;
+                }else return true;
+            }
+        },
+        methods:{
+            modifyProfile(e){
+                e.preventDefault();
+                const userId = JSON.parse(sessionStorage.userAuth).userId;
+                const token = JSON.parse(sessionStorage.userAuth).token;
+                const vm = this;
+                this.message = null;
+                this.error.global = null;
+                this.error.firstname = null;
+                this.error.lastname = null;
+
+                if(!this.formData.firstname){
+                    this.error.firstname = 'Vous devez entrer votre prénom !';
+                }else this.validText(this.formData.firstname, 'firstname');
+
+                if(!this.formData.lastname){
+                    this.error.lastname = 'Vous devez entrer votre nom !';
+                }else this.validText(this.formData.lastname, 'lastname');
+                
+                if(!this.error.firstname && !this.error.lastname){
+                    let data = new FormData();
+                    data.append('avatar', this.formData.file);
+                    data.append('firstname', this.formData.firstname);
+                    data.append('lastname', this.formData.lastname);
+                    data.append('service', this.formData.service);
+
+                    axios({
+                            url: `${config.urlApi}/api/auth/profile/${userId}`,
+                            method: 'PUT',
+                            headers: { 
+                                'Accept': 'application/json',
+                                'Content-Type': 'multipart/form-data',
+                                'authorization' : `Bearer ${token}`
+                            },
+                            data: data                                          
+                        })
+                        .then(function(res){
+                            vm.message = res.data.message;   
+                            vm.reload += 1;                   
+                        })
+                        .catch(function(error){
+                            let errormessage = error.response.data.error;
+                            vm.error.global = errormessage;
+                        });
+                }
+            },
+            validText(text, input){
+                const regexText = new RegExp(/^[a-zA-Z\s'\-àáâãäæçèéêëìíîïñòóôõöùúûüýÿœÀÁÂÃÄÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÜŒ]+$/g);
+                if(text.match(regexText) && text[0] != ' ' && text.length <= 25){
+                    this.valid[input] = true;
+                    this.error[input] = null;
+                    return true;
+                }else{
+                    this.valid[input] = false;
+                    if(text.length > 25){this.error[input] = 'Longueur du champ dépassée !';}
+                    else this.error[input] = 'Champ invalide !';
+                    return false;
+                }
+            },
+            upload(){
+                this.formData.file = this.$refs.file.files[0];
+            }
+        },
+        created(){
+            if(!sessionStorage.userAuth){
+                document.location.href = '/login';
+            }else this.session = true;
+
+            const userId = JSON.parse(sessionStorage.userAuth).userId;
+            const token = JSON.parse(sessionStorage.userAuth).token;
+
+            axios.get(`${config.urlApi}/api/auth/profile/${userId}`,{
+                    headers:{'authorization' : `Bearer ${token}`}
+                })
+                .then(res =>{
+                    this.formData = {...res.data};
+                })
+                .catch(error=> {
+                    if(error.response.status > 400){
+                        document.location.href = `/error/${error.response.status}`;
+                    }
+                });   
+        }    
+    }
+</script>
+
+<style scoped>
+    .div-global{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    section{
+        width: 95%;
+        margin-top: 20px;   
+    }
+
+    section div{
+        background-color: #333;
+        padding: 5px;
+         
+    }
+
+    .div-delete{
+        margin-top: 20px;
+        padding-top: 20px;
+        padding-bottom: 20px;
+    }
+
+    h2{
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+
+    form{
+        display: flex;
+        flex-direction: column;
+        line-height: 40px;
+        font-size: 1.2rem;
+    }
+
+    input, select{
+        height: 30px;
+        font-size: 1.2rem;
+        margin-bottom: 10px;    
+    }
+
+    .btnSubmit{
+        margin-top: 20px;
+        height: 40px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    .btnSubmit:hover:not(:disabled){
+        background-color: #a3cfbb;
+        transition: 400ms;
+    }
+
+    .btnSubmit:disabled{
+        cursor: not-allowed;
+    }
+
+    .btn-delete{
+        width: 100%;
+        height: 40px;
+        background-color: #f1aeb5;
+        font-size: 1.2rem;
+        border: none;
+        border-radius: 5px;
+    }
+
+    .btn-delete:hover {
+        color: #fff;
+        background-color: #842029;
+        transition: 400ms;
+    }
+    
+    .success{
+        color: #a3cfbb;
+    }
+
+    .error{
+        color: #f1aeb5;
+    }
+
+    .valid{
+        background-color: #a3cfbb;
+    }
+
+    @media screen and (min-width: 1024px) {
+        .div-global{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+            align-items: start;
+        }
+
+        section{
+            width: 67%;
+            margin-top: 0px
+        }
+    }
+</style>
